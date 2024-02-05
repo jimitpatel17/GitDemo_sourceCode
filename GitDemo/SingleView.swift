00d662499bd455503892fa63cf8948004cc2531b
jimitpatel17 <jimit56785@gmail.com>
2024-02-05 23:35:59 +0530

    Addign text.

---
+++ GitDemo/SingleView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleView.swift
+//  GitDemo
+//
+//  Created by jimit patel on 05/02/24.
+//
+
+import SwiftUI
+
+struct SingleView: View {
+    var body: some View {
+        Text("Shree Ganesh ji!!")
+    }
+}
+
+#Preview {
+    SingleView()
+}
