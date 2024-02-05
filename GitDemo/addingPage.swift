f56e043577c2ee393fb9c870ddcf20132024c760
jimitpatel17 <jimit56785@gmail.com>
2024-02-05 23:48:19 +0530

    changing text

---
+++ GitDemo/addingPage.swift
@@ -0,0 +1,18 @@
+//
+//  addingPage.swift
+//  GitDemo
+//
+//  Created by jimit patel on 05/02/24.
+//
+
+import SwiftUI
+
+struct addingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addingPage()
+}
