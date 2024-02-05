8f0582dc746da6eccad143ae5636556c86df1a07
jimitpatel17 <jimit56785@gmail.com>
2024-02-05 23:13:56 +0530

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by jimit patel on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
